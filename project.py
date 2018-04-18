74b18a4cbe9ade12b22382c8737f09ae45905dd2
Pratirodh Prasai <32813621+dexter800@users.noreply.github.com>
2018-04-19 00:43:25 +1000

    Add files via upload

---
+++ project.py
@@ -0,0 +1,16 @@
+import urllib.request
+
+def read_text():
+    quotes= open("movie_quotes.txt")
+    contents_of_files = quotes.read()
+    print(contents_of_files)
+    quotes.close()
+    check_profanity(contents_of_files)
+
+    
+def check_profanity(text_to_check):
+    connection= urllib.request.urlopen("http://www.wdylike.appspot.com/?q="+ text_to_check)
+    out_put= connection.read()
+    print(out_put)
+    connection.close()
+read_text()
